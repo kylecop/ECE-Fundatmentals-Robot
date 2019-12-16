11e221bb255899e94456bb03c12cb2cb36b6a596
kylecop <43895134+kylecop@users.noreply.github.com>
2019-12-16 10:47:14 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+/* main.c - main routine goes here
+ * Author: Kyle Coppedge   
+ * Purpose: Project 1
+ * date: 1/11/2018
+ */
+
+#include <p30f3013.h>
+#include "aliases.h"
+#include "utilities.h"
+
+int main() {
+  /* initialize the ability to send messages to the
+   * PICKit 2 using printf.
+   */
+  initializeUART();
+
+  // put code here!
+  printf("ECE Rules!\r\n");
+  halt();
+  return(0);
+}
